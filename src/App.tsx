34362d8c4f7d2e5f7a618fa4a2d8ca714454a3e4
Nicholas Westerhausen <2317381+nwesterhausen@users.noreply.github.com>
2022-05-31 15:56:33 -0400

    feat: :fire: put header and path display into app
    Remove extra components -- it isn't clear how to inherit signals so just going to make monolithic app.tsx

--- src/App.tsx
+++ src/App.tsx
@@ -1,55 +1,71 @@
-import { Container, Tabs, Tab, Form, FormControl, Alert, Accordion, Button } from 'solid-bootstrap';
-import { emit, listen } from '@tauri-apps/api/event'
-import {open as tauriOpen, OpenDialogOptions} from '@tauri-apps/api/dialog'
-import { Accessor, Component, createMemo, createResource, createSignal } from 'solid-js';
-import HeaderBar from './components/HeaderBar';
+import { Container, Navbar, Nav, NavDropdown, Tabs, Tab, Form, FormControl, Alert, Button } from 'solid-bootstrap';
+import { listen } from '@tauri-apps/api/event'
+import { open as tauriOpen, OpenDialogOptions } from '@tauri-apps/api/dialog'
+import { Component, createEffect, createMemo, createResource, createSignal } from 'solid-js';
 import Listing from './components/Listing';
-import RawPathDisplay from './components/RawPathDisplay';
+import { init as initStore, get as getFromStore, set as saveToStore, SAVES_PATH } from './settings';
+import { LIB_VERSION } from './version';
+import { AiFillGithub } from 'solidjs-icons/ai';
 
 const openDialogOptions: OpenDialogOptions = {
   directory: true,
   title: "Select your current DF Save Folder (e.g. ...DF/data/saves)"
 }
 
-async function performTauriOpenDiaglog(source, { value, refetching}) {
-  try {
-    let folderPath = await tauriOpen(openDialogOptions);
-    if (Array.isArray(folderPath)) {
-      return folderPath[0];
-    }
-    return folderPath;
-  } catch(error) {
-    console.error(error);
-    return "";
-  }
-}
-
 function getSavePathFromWorldDat(dadpath: string, manpath: string) {
   let targetPath = dadpath;
   if (manpath && manpath !== "") {
-    targetPath = manpath; 
+    targetPath = manpath;
   }
   let pathDelimation = "/";
   if (targetPath.indexOf("\\") !== -1) {
     pathDelimation = "\\";
   }
   let pathArr = targetPath.split(pathDelimation);
-  while(pathArr[pathArr.length - 1] !== "save") {
-    pathArr = pathArr.slice(0, -1);  
+  while (pathArr[pathArr.length - 1] !== "save") {
+    pathArr = pathArr.slice(0, -1);
     if (pathArr.length === 0) {
       return [];
     }
   }
   return pathArr;
-  
+
 }
 
 const App: Component = () => {
   const [dragAndDropPath, setDragAndDropPath] = createSignal(""); // Path to the dropped file location
   const [doManualFolderSelect, setManualFolderSelect] = createSignal(false); // Change to true to perform open folder diaglog
-  const [manuallySpecifiedPath, { mutate, refetch }]  = createResource(doManualFolderSelect, performTauriOpenDiaglog)
+  const [manuallySpecifiedPath, { mutate, refetch }] = createResource(doManualFolderSelect, performTauriOpenDiaglog)
   const saveFolderPath = createMemo(() => getSavePathFromWorldDat(dragAndDropPath(), manuallySpecifiedPath()));
-  
+  createEffect(() => {
+    if (saveFolderPath().length) {
+      saveToStore(SAVES_PATH, saveFolderPath().join("/"));
+    }
+  })
+
+  async function performTauriOpenDiaglog(source, { value, refetching }) {
+    setManualFolderSelect(false);
+    try {
+      let folderPath = await tauriOpen(openDialogOptions);
+      if (Array.isArray(folderPath)) {
+        return folderPath[0];
+      }
+      return folderPath;
+    } catch (error) {
+      console.error(error);
+      return "";
+    }
+  }
+
+  initStore().then(() => {
+    return getFromStore(SAVES_PATH);
+  })
+    .then(val => {
+      if (val !== "") {
+        setDragAndDropPath(val);
+      }
+    })
+    .catch(console.error)
 
   // Listen for a file being dropped on the window to change the save location.
   listen("tauri://file-drop", (event) => {
@@ -58,14 +74,42 @@ const App: Component = () => {
 
 
   return (<>
-    <HeaderBar />
+    <Navbar>
+      <Container>
+        <Nav>
+          <NavDropdown title="Save Folder">
+            <NavDropdown.Header >{saveFolderPath().join("/")}</NavDropdown.Header>
+            <NavDropdown.Item onClick={() => {
+              setManualFolderSelect(true);
+            }}>Pick new folder..</NavDropdown.Item>
+          </NavDropdown>
+          <NavDropdown title="Change Save" id="basic-nav-dropdown" >
+            <NavDropdown.Item href="#action/3.1">current</NavDropdown.Item>
+            <NavDropdown.Item href="#action/3.1">region1</NavDropdown.Item>
+            <NavDropdown.Item active href="#action/3.1">region2</NavDropdown.Item>
+          </NavDropdown>
+        </Nav>
+        <Nav>
+          <ul class='list-inline'>
+
+            <li class='list-inline-item small'>
+              Version {LIB_VERSION}
+            </li>
+            <li class='list-inline-item'>
+              <a href="" target="_blank" class='link-dark icon-link'><AiFillGithub /></a>
+            </li>
+
+          </ul>
+        </Nav>
+      </Container>
+    </Navbar>
     <Container class='p-2'>
       {saveFolderPath().length == 0 ? <>
         <Alert variant="warning">
           <Alert.Heading>Dwarf Fortress save directory path is unset!</Alert.Heading>
           <p>
             To set the path to your Dwarf Fortress Save, drag and drop a <code>world.dat</code> file from
-            any of the saves in your save folder onto this window, or use the button below to pull up a folder 
+            any of the saves in your save folder onto this window, or use the button below to pull up a folder
             selection dialog.
           </p>
           <Container class='p-3'>
@@ -75,14 +119,6 @@ const App: Component = () => {
           </Container>
         </Alert>
       </> : <>
-        <Accordion flush>
-          <Accordion.Item eventKey="0">
-            <Accordion.Header>Current Saves Folder</Accordion.Header>
-            <Accordion.Body>
-              <RawPathDisplay path={saveFolderPath()} />
-            </Accordion.Body>
-          </Accordion.Item>
-        </Accordion>
         <Form class="d-flex">
           <FormControl
             type="search"
