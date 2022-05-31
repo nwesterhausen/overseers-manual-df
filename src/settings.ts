7b54e0303a71651ab1454a8be2e3ec037b9468e0
Nicholas Westerhausen <2317381+nwesterhausen@users.noreply.github.com>
2022-05-31 15:55:04 -0400

    feat: :heavy_plus_sign: add tauri store plugin

---
+++ src/settings.ts
@@ -0,0 +1,52 @@
+import { Store } from 'tauri-plugin-store-api';
+
+const store = new Store("settings.json");
+await store.load();
+const CURRENT_VERSION = 1;
+
+const DATA_VERSION = "dataVersion";
+export const SAVES_PATH = "dfSavesPath";
+export const LAST_SAVE = "lastSaveUses";
+
+
+export async function init() {
+    let keys = await store.keys();
+    console.log(keys);
+    let resave = false;
+    if (keys.indexOf(DATA_VERSION) === -1) {
+        console.log("Missing",DATA_VERSION);
+        await store.set(DATA_VERSION, CURRENT_VERSION);
+        resave = true;
+    }
+    if (keys.indexOf(SAVES_PATH) === -1) {
+        console.log("Missing",SAVES_PATH);
+        await store.set(SAVES_PATH, "")
+        resave = true;
+    }
+    if (keys.indexOf(LAST_SAVE) === -1) {
+        console.log("Missing",LAST_SAVE);
+        await store.set(LAST_SAVE, "")
+        resave = true;
+    }
+    if (resave) {
+        
+    await store.save();
+    }
+}
+
+export async function set(key: string, value: string) {
+    console.log(`Set ${key} to ${value} in store`);
+    await store.set(key, value);
+    await store.save();
+}
+
+export async function get(key: string): Promise<string> {
+    console.log(`Retrieve ${key} from store`);
+    return await store.get<string>(key);
+}
+
+export default {
+    init,
+    set,
+    get
+}
