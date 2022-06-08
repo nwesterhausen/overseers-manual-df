1b709087516b4fda2ed1a0891e0ac328dccabaa2
Nicholas Westerhausen <2317381+nwesterhausen@users.noreply.github.com>
2022-06-08 11:41:10 -0400

    style: :art: auto-format

--- src/settings.ts
+++ src/settings.ts
@@ -1,30 +1,30 @@
-import { Store } from "tauri-plugin-store-api";
+import { Store } from 'tauri-plugin-store-api';
 
-const store = new Store("settings.json");
+const store = new Store('settings.json');
 await store.load();
 const CURRENT_VERSION = 1;
 
-const DATA_VERSION = "dataVersion";
-export const SAVES_PATH = "dfSavesPath";
-export const LAST_SAVE = "lastSaveUsed";
+const DATA_VERSION = 'dataVersion';
+export const SAVES_PATH = 'dfSavesPath';
+export const LAST_SAVE = 'lastSaveUsed';
 
 export async function init() {
   let keys = await store.keys();
   console.log(keys);
   let resave = false;
   if (keys.indexOf(DATA_VERSION) === -1) {
-    console.log("Missing", DATA_VERSION);
+    console.log('Missing', DATA_VERSION);
     await store.set(DATA_VERSION, CURRENT_VERSION);
     resave = true;
   }
   if (keys.indexOf(SAVES_PATH) === -1) {
-    console.log("Missing", SAVES_PATH);
-    await store.set(SAVES_PATH, "");
+    console.log('Missing', SAVES_PATH);
+    await store.set(SAVES_PATH, '');
     resave = true;
   }
   if (keys.indexOf(LAST_SAVE) === -1) {
-    console.log("Missing", LAST_SAVE);
-    await store.set(LAST_SAVE, "");
+    console.log('Missing', LAST_SAVE);
+    await store.set(LAST_SAVE, '');
     resave = true;
   }
   if (resave) {
