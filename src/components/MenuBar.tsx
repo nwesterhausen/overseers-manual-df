62b117b032587528623732b0d3d51cd06ce4ad7b
Nicholas Westerhausen <2317381+nwesterhausen@users.noreply.github.com>
2022-08-07 15:29:41 -0400

    feat: ✨ add refresh button for parsed raws

--- src/components/MenuBar.tsx
+++ src/components/MenuBar.tsx
@@ -3,9 +3,11 @@ import { Component, For, Match, Switch } from 'solid-js';
 import { DIR_DF, DIR_NONE, DIR_SAVE, useDirectoryProvider } from '../providers/DirectoryProvider';
 import { BsFolderSymlinkFill } from 'solid-icons/bs';
 import { HiOutlineRefresh } from 'solid-icons/hi';
+import { STS_IDLE, useRawsProvider } from '../providers/RawsProvider';
 
 const MenuBar: Component = () => {
   const directoryContext = useDirectoryProvider();
+  const rawsContext = useRawsProvider();
 
   return (
     <Navbar variant='dark'>
@@ -47,14 +49,16 @@ const MenuBar: Component = () => {
           <NavItem class='mx-2'>
             <OverlayTrigger
               placement='auto'
-              overlay={<Tooltip id='refresh-button-tooltip'>Refresh save directories from current folder</Tooltip>}>
+              overlay={<Tooltip id='refresh-button-tooltip'>Refresh valid save directory options</Tooltip>}>
               <Button variant='outline-info' class='border-0' onClick={directoryContext.refreshSaveDirs}>
                 <HiOutlineRefresh class='icon-fix' />
               </Button>
             </OverlayTrigger>
           </NavItem>
 
-          <NavDropdown title='Change Save' menuVariant='dark'>
+          <NavDropdown
+            title={directoryContext.currentSave() === '' ? 'Choose Save' : directoryContext.currentSave()}
+            menuVariant='dark'>
             <For
               each={directoryContext.saveDirectoryOptions()}
               fallback={<NavDropdown.Header>No saves found in directory.</NavDropdown.Header>}>
@@ -67,6 +71,24 @@ const MenuBar: Component = () => {
               )}
             </For>
           </NavDropdown>
+
+          {directoryContext.currentSave() === '' ? (
+            <></>
+          ) : (
+            <NavItem class='mx-2'>
+              <OverlayTrigger
+                placement='auto'
+                overlay={<Tooltip id='refresh-button-tooltip'>Refresh the raws from the current save</Tooltip>}>
+                <Button
+                  disabled={rawsContext.currentStatus() !== STS_IDLE}
+                  variant='outline-info'
+                  class='border-0'
+                  onClick={() => rawsContext.setLoadRaws(true)}>
+                  <HiOutlineRefresh class='icon-fix' />
+                </Button>
+              </OverlayTrigger>
+            </NavItem>
+          )}
         </Nav>
       </Container>
     </Navbar>
