f9331486b0fd390167f52eeb97722c1cd0bf47fe
Nicholas Westerhausen <2317381+nwesterhausen@users.noreply.github.com>
2022-12-22 16:03:16 -0500

    feat: :sparkles: de-dupe parsed raws by identifier

--- src/definitions/Raw.ts
+++ src/definitions/Raw.ts
@@ -1,4 +1,5 @@
-import { Raw } from './types';
+import { AssignBasedOn, GenerateSearchString } from './Creature';
+import { Creature, Raw } from './types';
 
 /**
  * Array.prototype.filter function to only allow valid Raw objects.
@@ -34,3 +35,63 @@ export const RawsFirstLetters = (arr: Raw[]): string[] => {
     ),
   ].sort();
 };
+
+/**
+ * Returns a unique and sorted array of raws from raws
+ */
+export const UniqueSort = (rawsArray: Raw[]): Raw[] => {
+  // Sort all raws by name
+  const rawsSorted = rawsArray.filter(FilterInvalidRaws).sort((a, b) => (a.name < b.name ? -1 : 1));
+
+  // Loop over all sorted raws
+  for (let i = 0; i < rawsSorted.length; i++) {
+    if (!rawsSorted[i].raw_module_parents) {
+      // Initialize raw_module_parents
+      rawsSorted[i].raw_module_parents = [rawsSorted[i].raw_module_found_in];
+    }
+    switch (rawsSorted[i].raw_type) {
+      case "Creature":
+        // Assume its a creature raw (all we handle right now)
+        const creature = rawsSorted[i] as Creature;
+        // If its based on another raw, find it and apply it
+        if (creature.based_on && creature.based_on.length) {
+          const matches = rawsSorted.filter((c) => c.objectId === creature.based_on);
+          if (matches.length === 1) {
+            rawsSorted[i] = AssignBasedOn(creature, matches[0] as Creature);
+          } else {
+            console.warn(`${matches.length} matches for 'based_on':${creature.based_on}`);
+          }
+        }
+        // Build a search string for the raw
+        rawsSorted[i].searchString = GenerateSearchString(rawsSorted[i] as Creature);
+        break;
+      default:
+        console.error(`Unhandled raw type ${rawsSorted[i].raw_type}!`);
+    }
+  }
+
+  // Unique values only
+  const uniqResult = rawsSorted.reduce((res: Raw[], current) => {
+    // Check if we already have one with that identifier
+    const raw = res.find(v => v.identifier === current.identifier);
+
+    // If the identifier exists in our results array, we already have it entered. We can append our source to it.
+    if (raw) {
+
+      // Check if the array of module_parents exists or not first
+      if (Array.isArray(raw.raw_module_parents)) {
+        raw.raw_module_parents.push(current.raw_module_found_in);
+      } else {
+        raw.raw_module_parents = [current.raw_module_found_in];
+      }
+
+      return res;
+    } else {
+      // If we didn't find it, append it
+      return [...res, current];
+    }
+  }, [] as Raw[]);
+
+  // Return the sorted and unique result
+  return uniqResult;
+}
