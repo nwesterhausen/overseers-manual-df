3d1aa1c9c1c6f4f02bb5d00ea7af9938848977ac
Nicholas Westerhausen <2317381+nwesterhausen@users.noreply.github.com>
2022-12-23 08:53:09 -0500

    fix: 🐛 always show the loading message

--- src/providers/RawsProvider.tsx
+++ src/providers/RawsProvider.tsx
@@ -2,7 +2,7 @@ import { createContextProvider } from '@solid-primitives/context';
 import { invoke } from '@tauri-apps/api';
 import { appWindow } from '@tauri-apps/api/window';
 import { createEffect, createMemo, createResource, createSignal } from 'solid-js';
-import { RawsFirstLetters, UniqueSort } from '../definitions/Raw';
+import { UniqueSort } from '../definitions/Raw';
 import type { Creature, ProgressPayload, Raw } from '../definitions/types';
 import { useDirectoryProvider } from './DirectoryProvider';
 import { useSearchProvider } from './SearchProvider';
@@ -19,11 +19,6 @@ export const [RawsProvider, useRawsProvider] = createContextProvider(() => {
 
   // Signal for setting raw parse status
   const [parsingStatus, setParsingStatus] = createSignal(STS_IDLE);
-
-  // Log the effect changes
-  createEffect(() => {
-    console.log(parsingStatus());
-  });
 
   // Signal for loading raws
   const [loadRaws, setLoadRaws] = createSignal(false);
@@ -54,11 +49,6 @@ export const [RawsProvider, useRawsProvider] = createContextProvider(() => {
     });
   });
 
-  // The alphabet but only the letters for which we have entries.
-  const allRawsAlphabet = createMemo((): string[] => {
-    return RawsFirstLetters(allRawsJsonSearchFiltered() as Raw[]);
-  });
-
   // Signal for raw parsing progress
   const [parsingProgress, setParsingProgress] = createSignal<ProgressPayload>({ current_module: '', percentage: 0.0 });
 
@@ -70,7 +60,7 @@ export const [RawsProvider, useRawsProvider] = createContextProvider(() => {
       ({ event, payload }) => {
         const progress = payload as ProgressPayload;
         setParsingProgress(progress);
-        console.log(payload);
+        console.debug(payload);
       }
     )
     .then(() => {
@@ -92,8 +82,6 @@ export const [RawsProvider, useRawsProvider] = createContextProvider(() => {
     () => allRawsJsonSearchFiltered().filter((x) => x.raw_type === 'Creature') as Creature[]
   );
 
-  const creatureRawsAlphabet = createMemo(() => RawsFirstLetters(creatureRaws()));
-
   async function parseRaws(): Promise<Raw[]> {
     const dir = directoryContext.directoryPath().join('/');
 
@@ -105,7 +93,8 @@ export const [RawsProvider, useRawsProvider] = createContextProvider(() => {
       );
 
       setParsingStatus(STS_LOADING);
-      setParsingProgress({ current_module: '', percentage: 1.0 });
+      await new Promise((resolve) => setTimeout(resolve, 1));
+      setParsingProgress({ current_module: '', percentage: 0.0 });
 
       // Flatten the array of arrays
       const result = raw_file_data.flat();
@@ -137,8 +126,6 @@ export const [RawsProvider, useRawsProvider] = createContextProvider(() => {
     allRawsJsonSearchFiltered,
     setLoadRaws,
     parsingProgress,
-    allRawsAlphabet,
     creatureRaws,
-    creatureRawsAlphabet,
   };
 });
