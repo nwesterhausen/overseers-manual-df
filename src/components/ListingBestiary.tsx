16d6fc34f78c1e31f126d695f3010f900241781d
Nicholas Westerhausen <2317381+nwesterhausen@users.noreply.github.com>
2022-12-23 08:55:52 -0500

    perf: 🔥 remove dead code

--- src/components/ListingBestiary.tsx
+++ src/components/ListingBestiary.tsx
@@ -1,21 +1,11 @@
-import { Component, For, createMemo, createSignal } from 'solid-js';
+import { Component, For } from 'solid-js';
 import { isCreature } from '../definitions/Creature';
 import type { Creature } from '../definitions/types';
 import { useRawsProvider } from '../providers/RawsProvider';
-import { useSearchProvider } from '../providers/SearchProvider';
 import CreatureListing from './CreatureListing';
 
 const ListingBestiary: Component = () => {
   const rawsContext = useRawsProvider();
-  const searchContext = useSearchProvider();
-
-  const secretId = `list${Math.floor(Math.random() * 100)}`;
-
-  // Filter the active key around
-  const [activeKey, setActiveKey] = createSignal(`${secretId}-${rawsContext.creatureRawsAlphabet()[0]}`);
-  const selectedLetter = createMemo(() => {
-    return activeKey().split('-')[1];
-  });
 
   return (
     <div class='row justify-content-center gap-3'>
