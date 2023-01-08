6df66cd4647c30509f0fab0126da27daeeb4c67d
Nicholas Westerhausen <2317381+nwesterhausen@users.noreply.github.com>
2023-01-08 09:02:07 -0500

    feat: ✨ add material types, state names and state colors to plant search string

--- src/definitions/Utils.ts
+++ src/definitions/Utils.ts
@@ -1,4 +1,4 @@
-import type { CasteRange } from '../definitions/types';
+import type { CasteRange, StateName } from '../definitions/types';
 
 export const M3_to_CM3 = 1000000;
 
@@ -168,39 +168,6 @@ const SpecificTickToCalendarConversion = (gameTicks: number, defined_spans: Game
   return outStr.join(', ');
 };
 
-/**
- * Check if an element is out of the viewport
- * (c) 2018 Chris Ferdinandi, MIT License, https://gomakethings.com
- *
- * Modified 2022 Nick Westerhausen
- *
- * @param elem - The element to check
- * @returns A set of booleans for each side of the element
- */
-export const isOutOfViewport = function (elem: HTMLElement): ElemViewportBounding {
-  // Get element's bounding
-  const bounding: DOMRect = elem.getBoundingClientRect();
-
-  // Check if it's out of the viewport on each side
-  const out = {
-    top: bounding.top < 0,
-    left: bounding.left < 0,
-    bottom: bounding.bottom > (window.innerHeight || document.documentElement.clientHeight),
-    right: bounding.right > (window.innerWidth || document.documentElement.clientWidth),
-    any: false,
-    all: false,
-  };
-  out.any = out.top || out.left || out.bottom || out.right;
-  out.all = out.top && out.left && out.bottom && out.right;
-
-  return out;
-};
-
-export type ElemViewportBounding = {
-  top: boolean;
-  left: boolean;
-  bottom: boolean;
-  right: boolean;
-  any: boolean;
-  all: boolean;
-};
+export function StatesIntoFlatArray(states: StateName): string[] {
+  return [states.solid, states.liquid, states.gas];
+}
