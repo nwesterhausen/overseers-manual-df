b2996b477d6e9b83b9673d754d773199d472b7f0
Nicholas Westerhausen <2317381+nwesterhausen@users.noreply.github.com>
2023-01-10 14:41:25 -0500

    feat: ✨ add some basic settings to settings page

--- src/components/filtering/SettingsModal.tsx
+++ src/components/filtering/SettingsModal.tsx
@@ -1,15 +1,108 @@
-import { Modal } from "solid-bootstrap";
-import { Component } from "solid-js";
-import { useSettingsContext } from "../../providers/SettingsProvider";
+import { getTauriVersion } from '@tauri-apps/api/app';
+import { Button, Form, Modal } from 'solid-bootstrap';
+import { Component, For, Show, createResource } from 'solid-js';
+import { useDirectoryProvider } from '../../providers/DirectoryProvider';
+import { useSettingsContext } from '../../providers/SettingsProvider';
 
 const SettingsModal: Component = () => {
-    const [settings, { handleClose }] = useSettingsContext();
+  const [settings, { handleClose }] = useSettingsContext();
+  const directoryContext = useDirectoryProvider();
 
-    return (
-        <Modal fullscreen onHide={handleClose} show={settings.show}>
-            <Modal.Header closeButton>Settings</Modal.Header>
-        </Modal>
-    )
-}
+  const [appInfo] = createResource(
+    async (): Promise<string> => {
+      const tauriVersion = await getTauriVersion();
+
+      return ' ' + tauriVersion;
+    },
+    { initialValue: '' }
+  );
+
+  return (
+    <Modal fullscreen onHide={handleClose} show={settings.show}>
+      <Modal.Header closeButton>Settings</Modal.Header>
+      <Modal.Body class='settings-window'>
+        <section>
+          <legend>Parsing Options</legend>
+          <em>Note: not currently able to be changed.</em>
+
+          <Form.Group controlId='settingsEnableVanilla'>
+            <Form.Check
+              checked
+              disabled
+              type='checkbox'
+              label={
+                <p>
+                  Parse vanilla raw modules from <code>$DF_DIR/data/vanilla</code>
+                </p>
+              }
+            />
+          </Form.Group>
+
+          <Form.Group controlId='settingsEnableInstalled'>
+            <Form.Check
+              checked
+              disabled
+              type='checkbox'
+              label={
+                <p>
+                  Parse installed raw modules from <code>$DF_DIR/data/installed_mods</code>
+                </p>
+              }
+            />
+          </Form.Group>
+          <Form.Group controlId='settingsEnableWorkshop'>
+            <Form.Check
+              checked
+              disabled
+              type='checkbox'
+              label={
+                <p>
+                  Parse workshop raw modules from <code>$DF_DIR/mods</code>
+                </p>
+              }
+            />
+          </Form.Group>
+        </section>
+        <section>
+          <legend>Directories</legend>
+          <p>
+            Current Dwarf Fortress Directory:{' '}
+            <code>
+              <Show when={directoryContext.directoryHistory().length > 0} fallback='None set'>
+                {directoryContext.currentDirectory().path.join('/')}
+              </Show>
+            </code>
+          </p>
+          <p>
+            <Show when={directoryContext.directoryHistory().length > 1} fallback={<em>No previous directories</em>}>
+              Previous directories:{' '}
+              <ul>
+                <For each={directoryContext.directoryHistory().slice(1, -1)}>
+                  {(dirSelection, index) => (
+                    <li>
+                      {dirSelection.path.join('/')}{' '}
+                      <Button
+                        onClick={() => directoryContext.promoteDirectoryFromHistory(index())}
+                        variant='outline-primary'>
+                        Promote
+                      </Button>
+                    </li>
+                  )}
+                </For>
+              </ul>
+            </Show>
+          </p>
+        </section>
+        <section>
+          <legend>About</legend>
+          <p>
+            This app was made with Tauri{appInfo} and SolidJS. The parsing is accomplished with the{' '}
+            <code>dfraw_json_parser</code> library.
+          </p>
+        </section>
+      </Modal.Body>
+    </Modal>
+  );
+};
 
 export default SettingsModal;
