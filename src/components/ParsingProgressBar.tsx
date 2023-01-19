daf5cc8bf3c2888481802bfc5cb70cfcb60fc7b4
Nicholas Westerhausen <2317381+nwesterhausen@users.noreply.github.com>
2023-01-19 11:44:13 -0500

    feat: ✨ improve parsing detail display

--- src/components/ParsingProgressBar.tsx
+++ src/components/ParsingProgressBar.tsx
@@ -1,4 +1,4 @@
-import { ProgressBar, Stack } from 'solid-bootstrap';
+import { ProgressBar } from 'solid-bootstrap';
 import { Component, Show, createMemo } from 'solid-js';
 import { STS_PARSING, useRawsProvider } from '../providers/RawsProvider';
 
@@ -12,17 +12,22 @@ const ParsingProgressBar: Component = () => {
   });
   return (
     <Show when={rawsContext.parsingStatus() === STS_PARSING}>
-      <Stack class='justify-content-center d-flex' gap={3}>
-        <div class='row align-items-start'>
-          <div class='col'>{`${rawsContext.parsingProgress().currentTask} ${percentage()}% Completed`}</div>
-          <div class='col'>Location {rawsContext.parsingProgress().currentLocation}</div>
-          <div class='col'>Module {rawsContext.parsingProgress().currentModule}</div>
-          <div class='col'>File {rawsContext.parsingProgress().currentFile}</div>
-        </div>
-        <Stack direction='vertical' gap={3}>
+      <div class='justify-content-center d-flex vstack'>
+        <div class='mt-3 gap-3 vstack'>
           <ProgressBar now={percentage()} animated />
-        </Stack>
-      </Stack>
+          <div class='row progress-details'>
+            <div class='col-auto'>
+              <span class='badge bg-primary'>Location</span> {rawsContext.parsingProgress().currentLocation}
+            </div>
+            <div class='col-auto'>
+              <span class='badge bg-primary'>Module</span> {rawsContext.parsingProgress().currentModule}
+            </div>
+            <div class='col-auto'>
+              <span class='badge bg-primary'>File</span> {rawsContext.parsingProgress().currentFile}
+            </div>
+          </div>
+        </div>
+      </div>
     </Show>
   );
 };
