e6e47ee49c3793b4974f9501e68a97d0ed81c85d
Nicholas Westerhausen <2317381+nwesterhausen@users.noreply.github.com>
2023-01-22 11:36:12 -0500

    fix: 🐛 fix no directory set message not appearing

--- src/components/Listings.tsx
+++ src/components/Listings.tsx
@@ -1,6 +1,6 @@
 import { Accordion } from 'solid-bootstrap';
 import { Component, For, Show } from 'solid-js';
-import { useRawsProvider } from '../providers/RawsProvider';
+import { STS_IDLE, useRawsProvider } from '../providers/RawsProvider';
 import { useSettingsContext } from '../providers/SettingsProvider';
 import DynamicAccordion from './DynamicAccordion';
 import DynamicCard from './DynamicCard';
@@ -11,29 +11,23 @@ const Listings: Component = () => {
   const [settings] = useSettingsContext();
 
   return (
-    <>
+    <Show
+      when={rawsContext.parsingStatus() === STS_IDLE && rawsContext.searchFilteredRaws().length > 0}
+      fallback={<div class='my-5 text-center fst-italic text-muted'>No results</div>}>
       <Pagination />
       <Show
         when={settings.displayStyleGrid}
         fallback={
           <Accordion class='ms-2 me-2'>
-            <For
-              each={rawsContext.searchFilteredRaws()}
-              fallback={<div class='text-center fst-italic text-muted'>No results</div>}>
-              {(raw) => <DynamicAccordion raw={raw} />}
-            </For>
+            <For each={rawsContext.searchFilteredRaws()}>{(raw) => <DynamicAccordion raw={raw} />}</For>
           </Accordion>
         }>
         <div class='row justify-content-center gap-3'>
-          <For
-            each={rawsContext.searchFilteredRaws()}
-            fallback={<div class='text-center fst-italic text-muted'>No results</div>}>
-            {(raw) => <DynamicCard raw={raw} />}
-          </For>
+          <For each={rawsContext.searchFilteredRaws()}>{(raw) => <DynamicCard raw={raw} />}</For>
         </div>
       </Show>
       <Pagination />
-    </>
+    </Show>
   );
 };
 
