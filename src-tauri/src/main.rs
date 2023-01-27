865ddfe1f1569eae6a5a4d90f548737aadd926f5
Nicholas Westerhausen <2317381+nwesterhausen@users.noreply.github.com>
2023-01-27 10:24:45 -0500

    refactor: ✨ add helper function for backend

--- src-tauri/src/main.rs
+++ src-tauri/src/main.rs
@@ -5,9 +5,6 @@
 
 use tauri_plugin_log::LogTarget;
 
-// use tauri::{AppHandle, Runtime};
-// use fern::colors::{Color, ColoredLevelConfig};
-
 #[tauri::command]
 #[allow(clippy::needless_pass_by_value)]
 /// Passthru to parse all raws at game path
@@ -22,45 +19,28 @@ fn parse_raws_in_module_location(module_location: &str, window: tauri::window::W
     dfraw_json_parser::parse_location_with_tauri_emit(&module_location, window)
 }
 
-// #[tauri::command]
-// #[allow(clippy::needless_pass_by_value)]
-// /// Passthru to parse all raws in raw location
-// fn parse_raws_in_raw_module(raw_module_path: &str, window: tauri::window::Window) -> String {
-//     dfraw_json_parser::parse_raw_module_with_tauri_emit(&module_location, &window)
-// }
-
-// #[tauri::command]
-// #[allow(clippy::needless_pass_by_value)]
-// /// Passthru to parse all raws in raw location
-// fn parse_single_raw_file(raw_file_path: &str, window: tauri::window::Window) -> String {
-//     dfraw_json_parser::parse_raw_file_with_tauri_emit(&module_location, &window)
-// }
-
 #[tauri::command]
 /// Passthru to parse all info.txt files at game path
 fn parse_all_raws_info(path: &str) -> String {
     dfraw_json_parser::parse_info_txt_in_game_dir(&path)
 }
 
-// #[tauri::command]
-// /// Passthru to parse all info.txt files at location
-// fn parse_raws_info_in_location(path: &str) -> String {
-//     dfraw_json_parser::parse_info_txt_in_location(&path)
-// }
-
-// #[tauri::command]
-// /// Passthru to parse the info.txt for a specific module
-// fn parse_raws_info_in_module(path: &str) -> String {
-//     dfraw_json_parser::parse_info_txt_in_module(&path)
-// }
+#[tauri::command]
+#[allow(clippy::needless_pass_by_value)]
+/// Passthru to parse all raws in raw location
+fn parse_raws_in_locations(module_locations: Vec<String>, window: tauri::window::Window) -> String {
+    let mut results: Vec<String> = vec![];
+    for module_location in module_locations {
+        let cloned_window = window.clone();
+        results.push(dfraw_json_parser::parse_location_with_tauri_emit(
+            &module_location,
+            cloned_window,
+        ));
+    }
+    format!("[{}]", results.join(","))
+}
 
 fn main() {
-    // Setup logging
-    // // Specify color configuration
-    // let colors = ColoredLevelConfig::new()
-    //     // Specify info as cyan
-    //     .info(Color::Cyan);
-
     // Launch the app
     let app = tauri::Builder::default()
         // Add simple storage plugin
@@ -86,6 +66,7 @@ fn main() {
             parse_all_raws,
             parse_raws_in_module_location,
             parse_all_raws_info,
+            parse_raws_in_locations,
         ])
         .run(tauri::generate_context!());
 
