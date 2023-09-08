4afb85becb532cdc10bde0ebf8acd75655f1af7d
Nicholas Westerhausen <2317381+nwesterhausen@users.noreply.github.com>
2023-09-08 08:25:29 -0400

    feat: add tailwindcss and update lint

--- src/components/menu/OpenSettingsButton.tsx
+++ src/components/menu/OpenSettingsButton.tsx
@@ -4,7 +4,7 @@ import { Component } from 'solid-js';
 import { useSettingsContext } from '../../providers/SettingsProvider';
 
 const OpenSettingsButton: Component = () => {
-  const [settings, { handleOpen }] = useSettingsContext();
+  const [_settings, { handleOpen }] = useSettingsContext();
   return (
     <OverlayTrigger placement='auto' overlay={<Tooltip>Settings</Tooltip>}>
       <Button class='border-0 p-1' variant='outline-secondary' onClick={handleOpen}>
--- src/components/menu/TagRestrictionButton.tsx
+++ src/components/menu/TagRestrictionButton.tsx
@@ -4,7 +4,7 @@ import { Component } from 'solid-js';
 import { useSearchProvider } from '../../providers/SearchProvider';
 
 const TagRestrictionButton: Component<{ disabled: boolean }> = (props) => {
-  const searchContext = useSearchProvider();
+  const _searchContext = useSearchProvider();
   return (
     <OverlayTrigger placement='bottom' overlay={<Tooltip>Show Only a Specific Tag</Tooltip>}>
       <Button disabled={props.disabled} variant='outline-secondary' class='border-0 p-1 ms-1'>
