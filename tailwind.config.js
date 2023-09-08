c18829899338ed10dd818032fa82f9113995bb93
Nicholas Westerhausen <2317381+nwesterhausen@users.noreply.github.com>
2023-09-08 08:48:10 -0400

    feat: add daisyui

--- tailwind.config.js
+++ tailwind.config.js
@@ -7,6 +7,21 @@ module.exports = {
   theme: {
     extend: {},
   },
-  plugins: [],
+  plugins: [
+    require('@tailwindcss/typography'),
+    require("daisyui")
+  ],
+  daisyui: {
+    themes: [
+      {
+        dark: {
+          ...require('daisyui/src/theming/themes')["[data-theme='dark']"],
+          'base-100': '#002b36',
+          'neutral': '#1c1c1c',
+        }
+      },
+      'light',
+    ],
+  }
 }
 
