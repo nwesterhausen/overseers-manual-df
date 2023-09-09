ab022bf5ca23cc0e4cfb2a55bc2f57898b0f9f26
Nicholas Westerhausen <2317381+nwesterhausen@users.noreply.github.com>
2023-09-09 17:21:45 -0400

    fix: correct layout of toggles in raw type dropdown

--- src/components/menu/RawTypeCheckboxes.tsx
+++ src/components/menu/RawTypeCheckboxes.tsx
@@ -14,32 +14,34 @@ const RawTypeCheckboxes: Component<{ disabled: boolean }> = (props) => {
           id='dropdown-basic'>
           <TbTrees size={'1.5rem'} />
         </summary>
-        <ul class='p-2 shadow menu dropdown-content bg-base-100 rounded-box w-52 z-[1]'>
-          <li>
-            <a onClick={searchContext.handleToggleRequireCreature}>
-              <label class='cursor-pointer label'>
-                <span class='label-text'>Creatures</span>
-                <input type='checkbox' class='toggle toggle-xs' checked={searchContext.requireCreature()} />
-              </label>
-            </a>
-          </li>
-          <li>
-            <a onClick={searchContext.handleToggleRequirePlant}>
-              <label class='cursor-pointer label'>
-                <span class='label-text'>Plants</span>
-                <input type='checkbox' class='toggle toggle-xs' checked={searchContext.requirePlant()} />
-              </label>
-            </a>
-          </li>
-          <li>
-            <a onClick={searchContext.handleToggleRequireInorganic}>
-              <label class='cursor-pointer label'>
-                <span class='label-text'>Inorganics</span>
-                <input type='checkbox' class='toggle toggle-xs' checked={searchContext.requireInorganic()} />
-              </label>
-            </a>
-          </li>
-        </ul>
+        <div class='p-2 shadow menu dropdown-content bg-base-100 rounded-box w-52 z-[1]'>
+          <div class='flex flex-col'>
+            <div>
+              <a onClick={searchContext.handleToggleRequireCreature}>
+                <label class='cursor-pointer label'>
+                  <span class='label-text'>Creatures</span>
+                  <input type='checkbox' class='toggle toggle-xs' checked={searchContext.requireCreature()} />
+                </label>
+              </a>
+            </div>
+            <div>
+              <a onClick={searchContext.handleToggleRequirePlant}>
+                <label class='cursor-pointer label'>
+                  <span class='label-text'>Plants</span>
+                  <input type='checkbox' class='toggle toggle-xs' checked={searchContext.requirePlant()} />
+                </label>
+              </a>
+            </div>
+            <div>
+              <a onClick={searchContext.handleToggleRequireInorganic}>
+                <label class='cursor-pointer label'>
+                  <span class='label-text'>Inorganics</span>
+                  <input type='checkbox' class='toggle toggle-xs' checked={searchContext.requireInorganic()} />
+                </label>
+              </a>
+            </div>
+          </div>
+        </div>
       </details>
     </div>
   );
