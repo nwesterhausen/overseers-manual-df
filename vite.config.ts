a5a13da63572c27a91304184e138d5f00f1c6dd8
Nicholas Westerhausen <2317381+nwesterhausen@users.noreply.github.com>
2023-09-10 18:48:04 -0400

    fix: remove remaining unecessary deps

--- vite.config.ts
+++ vite.config.ts
@@ -1,4 +1,3 @@
-import path from 'path';
 import { defineConfig } from 'vite';
 import solidPlugin from 'vite-plugin-solid';
 
@@ -6,11 +5,5 @@ export default defineConfig({
   plugins: [solidPlugin()],
   build: {
     target: 'esnext',
-  },
-  resolve: {
-    alias: {
-      '~bootstrap': path.resolve(__dirname, 'node_modules/bootstrap'),
-      '~bootswatch': path.resolve(__dirname, 'node_modules/bootswatch/dist'),
-    },
   },
 });
