9ac264223c19f907f6e41181771bc0c2cba45610
Nicholas Westerhausen <2317381+nwesterhausen@users.noreply.github.com>
2023-09-18 10:18:35 -0400

    feat: expand settings file to include all options

--- src/providers/SettingsProvider.tsx
+++ src/providers/SettingsProvider.tsx
@@ -1,9 +1,17 @@
 import { ParentComponent, createContext, useContext } from 'solid-js';
 import { createStore } from 'solid-js/store';
+import {
+  DISPLAY_GRAPHICS,
+  INCLUDE_INSTALLED,
+  INCLUDE_MODS,
+  INCLUDE_VANILLA,
+  LAYOUT_AS_GRID,
+  get,
+  set,
+} from '../settings';
 
 type SettingsStore = [
   {
-    show: boolean;
     displayStyleGrid: boolean;
     displayGraphics: boolean;
     includeLocationVanilla: boolean;
@@ -11,8 +19,6 @@ type SettingsStore = [
     includeLocationInstalledMods: boolean;
   },
   {
-    handleOpen: () => void;
-    handleClose: () => void;
     toggleDisplayGrid: () => void;
     toggleDisplayGraphics: () => void;
     toggleIncludeLocationVanilla: () => void;
@@ -23,7 +29,6 @@ type SettingsStore = [
 
 const SettingsContext = createContext<SettingsStore>([
   {
-    show: false,
     displayStyleGrid: true,
     displayGraphics: true,
     includeLocationVanilla: true,
@@ -31,12 +36,6 @@ const SettingsContext = createContext<SettingsStore>([
     includeLocationInstalledMods: true,
   },
   {
-    handleOpen() {
-      console.log('Un-initialized handleOpen');
-    },
-    handleClose() {
-      console.log('Un-initialized handleClose');
-    },
     toggleDisplayGrid() {
       console.log('Un-initialized settings provider.');
     },
@@ -57,7 +56,6 @@ const SettingsContext = createContext<SettingsStore>([
 
 export const SettingsProvider: ParentComponent = (props) => {
   const [state, setState] = createStore({
-    show: false,
     displayStyleGrid: true,
     displayGraphics: true,
     includeLocationVanilla: true,
@@ -68,29 +66,104 @@ export const SettingsProvider: ParentComponent = (props) => {
   const defaultSettings = [
     state,
     {
-      handleOpen() {
-        setState({ show: true });
-      },
-      handleClose() {
-        setState({ show: false });
-      },
       toggleDisplayGrid() {
         setState({ displayStyleGrid: !state.displayStyleGrid });
+        set(LAYOUT_AS_GRID, state.displayStyleGrid)
+          .then(() => {
+            console.log('Saved display style grid to', state.displayStyleGrid);
+          })
+          .catch((err) => {
+            console.error('Error saving display style grid as', state.displayStyleGrid);
+            console.error(err);
+          });
       },
       toggleDisplayGraphics() {
         setState({ displayGraphics: !state.displayGraphics });
+        set(DISPLAY_GRAPHICS, state.displayGraphics)
+          .then(() => {
+            console.log('Saved display graphics to', state.displayGraphics);
+          })
+          .catch((err) => {
+            console.error('Error saving display graphics as', state.displayGraphics);
+            console.error(err);
+          });
       },
       toggleIncludeLocationVanilla() {
         setState({ includeLocationVanilla: !state.includeLocationVanilla });
+        set(INCLUDE_VANILLA, state.includeLocationVanilla)
+          .then(() => {
+            console.log('Saved include vanilla to', state.includeLocationVanilla);
+          })
+          .catch((err) => {
+            console.error('Error saving include vanilla as', state.includeLocationVanilla);
+            console.error(err);
+          });
       },
       toggleIncludeLocationMods() {
         setState({ includeLocationMods: !state.includeLocationMods });
+        set(INCLUDE_MODS, state.includeLocationMods)
+          .then(() => {
+            console.log('Saved include mods to', state.includeLocationMods);
+          })
+          .catch((err) => {
+            console.error('Error saving include mods as', state.includeLocationMods);
+            console.error(err);
+          });
       },
       toggleIncludeLocationInstalledMods() {
         setState({ includeLocationInstalledMods: !state.includeLocationInstalledMods });
+        set(INCLUDE_INSTALLED, state.includeLocationInstalledMods)
+          .then(() => {
+            console.log('Saved include installed mods to', state.includeLocationInstalledMods);
+          })
+          .catch((err) => {
+            console.error('Error saving include installed mods as', state.includeLocationInstalledMods);
+            console.error(err);
+          });
       },
     },
   ];
+
+  get(LAYOUT_AS_GRID)
+    .then((val) => {
+      if (typeof val === 'boolean') {
+        setState({ displayStyleGrid: val });
+        console.log('Loaded saved value for displayStyleGrid', val);
+      }
+    })
+    .catch(console.error);
+  get(DISPLAY_GRAPHICS)
+    .then((val) => {
+      if (typeof val === 'boolean') {
+        setState({ displayGraphics: val });
+        console.log('Loaded saved value for displayGraphics', val);
+      }
+    })
+    .catch(console.error);
+  get(INCLUDE_INSTALLED)
+    .then((val) => {
+      if (typeof val === 'boolean') {
+        setState({ includeLocationInstalledMods: val });
+        console.log('Loaded saved value for includeLocationInstalledMods', val);
+      }
+    })
+    .catch(console.error);
+  get(INCLUDE_MODS)
+    .then((val) => {
+      if (typeof val === 'boolean') {
+        setState({ includeLocationMods: val });
+        console.log('Loaded saved value for includeLocationMods', val);
+      }
+    })
+    .catch(console.error);
+  get(INCLUDE_VANILLA)
+    .then((val) => {
+      if (typeof val === 'boolean') {
+        setState({ includeLocationVanilla: val });
+        console.log('Loaded saved value for includeLocationVanilla', val);
+      }
+    })
+    .catch(console.error);
 
   return <SettingsContext.Provider value={defaultSettings as SettingsStore}>{props.children}</SettingsContext.Provider>;
 };
--- src/settings.ts
+++ src/settings.ts
@@ -2,14 +2,28 @@ import { Store } from './plugins/store';
 import { DIR_DF, DIR_NONE, DIR_RAWS, DIR_SAVE } from './providers/DirectoryProvider';
 
 const store = new Store('settings.json');
-const CURRENT_VERSION = 2;
+const CURRENT_VERSION = 3;
 
 const DATA_VERSION = 'dataVersion';
 export const PATH_STRING = 'dfDirPath';
-export const LAST_SAVE = 'lastSaveUsed';
 export const PATH_TYPE = 'pathType';
+export const DISPLAY_GRAPHICS = 'displayGraphics';
+export const LAYOUT_AS_GRID = 'layoutAsGrid';
+export const INCLUDE_VANILLA = 'includeVanilla';
+export const INCLUDE_MODS = 'includeMods';
+export const INCLUDE_INSTALLED = 'includeInstalled';
 
-const ValidKeys = [DATA_VERSION, PATH_STRING, LAST_SAVE, PATH_TYPE];
+const ValidKeys = [
+  DATA_VERSION,
+  PATH_STRING,
+  PATH_TYPE,
+  DISPLAY_GRAPHICS,
+  LAYOUT_AS_GRID,
+  INCLUDE_VANILLA,
+  INCLUDE_MODS,
+  INCLUDE_INSTALLED,
+];
+const BooleanKeys = [DISPLAY_GRAPHICS, LAYOUT_AS_GRID, INCLUDE_VANILLA, INCLUDE_MODS, INCLUDE_INSTALLED];
 
 /**
  * Initializes the store. This creates any missing values after checking that the `DATA_VERSION` matches our current version.
@@ -19,19 +33,16 @@ export async function init() {
   const keys = await store.keys();
   console.log(keys);
   let reSave = false;
+  // Settings Version
   if (keys.indexOf(DATA_VERSION) === -1) {
     console.log('Missing', DATA_VERSION);
     await store.set(DATA_VERSION, CURRENT_VERSION);
     reSave = true;
   }
+  // String Values
   if (keys.indexOf(PATH_STRING) === -1) {
     console.log('Missing', PATH_STRING);
     await store.set(PATH_STRING, '');
-    reSave = true;
-  }
-  if (keys.indexOf(LAST_SAVE) === -1) {
-    console.log('Missing', LAST_SAVE);
-    await store.set(LAST_SAVE, '');
     reSave = true;
   }
   if (keys.indexOf(PATH_TYPE) === -1) {
@@ -39,7 +50,47 @@ export async function init() {
     await store.set(PATH_TYPE, '');
     reSave = true;
   }
+  // Boolean Values
+  if (keys.indexOf(DISPLAY_GRAPHICS) === -1) {
+    console.log('Missing', DISPLAY_GRAPHICS);
+    await store.set(DISPLAY_GRAPHICS, true);
+    reSave = true;
+  }
+  if (keys.indexOf(LAYOUT_AS_GRID) === -1) {
+    console.log('Missing', LAYOUT_AS_GRID);
+    await store.set(LAYOUT_AS_GRID, true);
+    reSave = true;
+  }
+  if (keys.indexOf(INCLUDE_VANILLA) === -1) {
+    console.log('Missing', INCLUDE_VANILLA);
+    await store.set(INCLUDE_VANILLA, true);
+    reSave = true;
+  }
+  if (keys.indexOf(INCLUDE_MODS) === -1) {
+    console.log('Missing', INCLUDE_MODS);
+    await store.set(INCLUDE_MODS, false);
+    reSave = true;
+  }
+  if (keys.indexOf(INCLUDE_INSTALLED) === -1) {
+    console.log('Missing', INCLUDE_INSTALLED);
+    await store.set(INCLUDE_INSTALLED, true);
+    reSave = true;
+  }
   if (reSave) {
+    // Set Defaults if version number increased
+    const version = await store.get<number>(DATA_VERSION);
+    if (version !== CURRENT_VERSION) {
+      console.log('Version mismatch, resetting settings');
+      await store.set(DATA_VERSION, CURRENT_VERSION);
+      await store.set(PATH_STRING, '');
+      await store.set(PATH_TYPE, '');
+      await store.set(DISPLAY_GRAPHICS, true);
+      await store.set(LAYOUT_AS_GRID, true);
+      await store.set(INCLUDE_VANILLA, true);
+      await store.set(INCLUDE_MODS, false);
+      await store.set(INCLUDE_INSTALLED, true);
+    }
+    // Save the store if we had to create any missing values
     await store.save();
   }
 }
@@ -52,11 +103,16 @@ export async function init() {
  * @param key - The key to store the data under.
  * @param value - The data to be stored
  */
-export async function set(key: string, value: string) {
+export async function set(key: string, value: string | boolean) {
   if (ValidKeys.indexOf(key) === -1) {
     return;
   }
-  if (!value || value.length === 0) {
+  if (typeof value === 'boolean') {
+    // Only store boolean values for boolean keys
+    if (BooleanKeys.indexOf(key) === -1) {
+      return;
+    }
+  } else if (!value || value.length === 0) {
     return;
   }
   console.log(`Set ${key} to ${value} in store`);
@@ -88,12 +144,17 @@ export async function clear(key: string) {
  * @param key - The key to retrieve data from.
  * @returns The value stored under that key
  */
-export async function get(key: string): Promise<string> {
+export async function get(key: string): Promise<string | boolean> {
   if (ValidKeys.indexOf(key) === -1) {
     return '';
   }
   if (store.has(key)) {
     console.log(`Retrieve ${key} from store`);
+    if (BooleanKeys.indexOf(key) !== -1) {
+      const val = await store.get<boolean>(key);
+      console.log(val);
+      return val;
+    }
     const val = await store.get<string>(key);
     console.log(val);
     return val;
