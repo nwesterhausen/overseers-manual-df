8683eb094086199d941fcdeebeb110a116b1ecd1
Nicholas Westerhausen <nwesterhausen@gmail.com>
2023-10-24 09:44:09 -0400

    feat: settings button closes settings

--- src/components/menu/OpenSettingsButton.tsx
+++ src/components/menu/OpenSettingsButton.tsx
@@ -1,11 +1,12 @@
-import { A } from '@solidjs/router';
+import { A, useLocation } from '@solidjs/router';
 import { BiRegularCog } from 'solid-icons/bi';
 import { Component } from 'solid-js';
 
 const OpenSettingsButton: Component = () => {
+  const location = useLocation();
   return (
-    <div class='tooltip tooltip-left' data-tip='Open Settings'>
-      <A class='btn btn-sm btn-circle btn-ghost fill-secondary' href='/settings'>
+    <div class='tooltip tooltip-left' data-tip={location.pathname === '/' ? 'Open Settings' : 'Close Settings'}>
+      <A class='btn btn-sm btn-circle btn-ghost fill-secondary' href={location.pathname === '/' ? '/settings' : '/'}>
         <BiRegularCog size={'1.5rem'} />
       </A>
     </div>
