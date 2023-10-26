aad91aa89473e9ad64ada9883900d3cfebe5c393
Nicholas Westerhausen <nwesterhausen@ntsupply.com>
2023-10-26 14:55:43 -0400

    docs: add documentation

--- src-tauri/build.rs
+++ src-tauri/build.rs
@@ -1,5 +1,7 @@
 fn main() {
     tauri_build::build();
     #[allow(clippy::expect_used)]
+    // Collect some build-time information. This is used to populate the About dialog.
+    // The information includes environment information and build dependencies.
     built::write_built_file().expect("Failed to acquire build-time information");
 }
