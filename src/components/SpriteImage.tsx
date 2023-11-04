05f6af0d2581df3785e60734333cb46ca57be032
Nicholas Westerhausen <nwesterhausen@gmail.com>
2023-11-04 18:56:57 -0400

    feat: cycle graphics images through available

--- src/components/SpriteImage.tsx
+++ src/components/SpriteImage.tsx
@@ -1,6 +1,6 @@
 import { convertFileSrc, invoke } from '@tauri-apps/api/primitives';
 import { BiRegularImageAlt } from 'solid-icons/bi';
-import { Component, Show, createMemo, createResource } from 'solid-js';
+import { Component, Show, createMemo, createResource, createSignal } from 'solid-js';
 import { GraphicsResults } from '../definitions/GraphicsResults';
 import { COMMAND_GET_GRAPHICS_FOR_IDENTIFIER } from '../lib/Constants';
 import { useSettingsContext } from '../providers/SettingsProvider';
@@ -38,90 +38,118 @@ const SpriteImage: Component<SpriteImageProps> = (props) => {
     },
     {
       initialValue: {
-        graphic: undefined,
+        matchingGraphics: undefined,
         tilePages: [],
       },
     },
   );
 
-  const spriteDetails = createMemo((): SpriteImageDetail => {
-    if (typeof graphics.latest.graphic === 'undefined' || graphics.latest.tilePages.length === 0) {
-      return {
-        graphicFilePath: '',
-        offset: { x: 0, y: 0 },
-        offset2: { x: 0, y: 0 },
-        tileDim: { x: 0, y: 0 },
-        pageDim: { x: 0, y: 0 },
-      };
+  const [currentIndex, setCurrentIndex] = createSignal(0);
+
+  const spriteDetails = createMemo((): SpriteImageDetail[] => {
+    if (typeof graphics.latest.matchingGraphics === 'undefined' || graphics.latest.tilePages.length === 0) {
+      return [
+        {
+          graphicFilePath: '',
+          offset: { x: 0, y: 0 },
+          offset2: { x: 0, y: 0 },
+          tileDim: { x: 0, y: 0 },
+          pageDim: { x: 0, y: 0 },
+        },
+      ];
     }
-    // technically this is different for each isn't it...
-    if (graphics.latest.graphic.sprites && graphics.latest.graphic.sprites.length > 0) {
-      const sprite = graphics.latest.graphic.sprites[0];
-      const tilePage = graphics.latest.tilePages.find((tp) => tp.identifier === sprite.tilePageId);
-      return {
-        graphicFilePath: tilePage.file,
-        offset: sprite.offset,
-        offset2: sprite.offset2,
-        tileDim: tilePage.tileDim,
-        pageDim: tilePage.pageDim,
-      };
-    } else if (graphics.latest.graphic.layers.length > 0) {
-      const layer = graphics.latest.graphic.layers[0][1][0];
-      const tilePage = graphics.latest.tilePages.find((tp) => tp.identifier === layer.tilePageId);
-      return {
-        graphicFilePath: tilePage.file,
-        offset: layer.offset,
-        offset2: layer.offset2,
-        tileDim: tilePage.tileDim,
-        pageDim: tilePage.pageDim,
-      };
+    const results: SpriteImageDetail[] = [];
+    for (const graphic of graphics.latest.matchingGraphics) {
+      // technically this is different for each isn't it...
+      if (graphic.sprites && graphic.sprites.length > 0) {
+        for (const sprite of graphic.sprites) {
+          const tilePage = graphics.latest.tilePages.find((tp) => tp.identifier === sprite.tilePageId);
+          if (tilePage.file.length > 0) {
+            results.push({
+              graphicFilePath: tilePage.file,
+              offset: sprite.offset,
+              offset2: sprite.offset2,
+              tileDim: tilePage.tileDim,
+              pageDim: tilePage.pageDim,
+            });
+          }
+        }
+      } else if (graphic.layers.length > 0) {
+        const layer = graphic.layers[0][1][0];
+        const tilePage = graphics.latest.tilePages.find((tp) => tp.identifier === layer.tilePageId);
+        if (tilePage.file.length > 0) {
+          results.push({
+            graphicFilePath: tilePage.file,
+            offset: layer.offset,
+            offset2: layer.offset2,
+            tileDim: tilePage.tileDim,
+            pageDim: tilePage.pageDim,
+          });
+        }
+      }
     }
+    return results;
   });
 
+  setInterval(() => {
+    if (spriteDetails().length > 0) {
+      if (currentIndex() + 1 >= spriteDetails().length) {
+        setCurrentIndex(0);
+      } else {
+        setCurrentIndex(currentIndex() + 1);
+      }
+    } else {
+      setCurrentIndex(0);
+    }
+  }, 5 * 1000);
+
   const assetUrl = createMemo(() => {
-    if (spriteDetails().graphicFilePath.length > 0) {
-      const filePath = spriteDetails().graphicFilePath.replace(/\\/g, '/');
-      const assetUrl = convertFileSrc(filePath);
-      return assetUrl;
+    if (spriteDetails().length > 0) {
+      if (spriteDetails()[currentIndex()].graphicFilePath.length > 0) {
+        const filePath = spriteDetails()[currentIndex()].graphicFilePath.replace(/\\/g, '/');
+        const assetUrl = convertFileSrc(filePath);
+        return assetUrl;
+      }
     }
     return '';
   });
 
   const offsetX = createMemo(() => {
-    let offX = spriteDetails().offset.x;
-    if (spriteDetails().offset2 && spriteDetails().offset2.x > offX) {
-      offX = spriteDetails().offset2.x;
+    let offX = spriteDetails()[currentIndex()].offset.x;
+    if (spriteDetails()[currentIndex()].offset2 && spriteDetails()[currentIndex()].offset2.x > offX) {
+      offX = spriteDetails()[currentIndex()].offset2.x;
     }
     return offX;
   });
   const offsetY = createMemo(() => {
-    let offY = spriteDetails().offset.y;
-    if (spriteDetails().offset2 && spriteDetails().offset2.y > offY) {
-      offY = spriteDetails().offset2.y;
+    let offY = spriteDetails()[currentIndex()].offset.y;
+    if (spriteDetails()[currentIndex()].offset2 && spriteDetails()[currentIndex()].offset2.y > offY) {
+      offY = spriteDetails()[currentIndex()].offset2.y;
     }
     return offY;
   });
   const dimX = createMemo(() => {
-    const scale = offsetX() - spriteDetails().offset.x + 1;
-    return spriteDetails().tileDim.x * scale;
+    const scale = offsetX() - spriteDetails()[currentIndex()].offset.x + 1;
+    return spriteDetails()[currentIndex()].tileDim.x * scale;
   });
   const dimY = createMemo(() => {
-    const scale = offsetY() - spriteDetails().offset.y + 1;
-    return spriteDetails().tileDim.y * scale;
+    const scale = offsetY() - spriteDetails()[currentIndex()].offset.y + 1;
+    return spriteDetails()[currentIndex()].tileDim.y * scale;
   });
   const positionOffset = createMemo(() => {
-    return `${spriteDetails().pageDim.x - (offsetX() + 1) * spriteDetails().tileDim.x + dimX()}px ${
-      spriteDetails().pageDim.y - (offsetY() + 1) * spriteDetails().tileDim.y + dimY()
+    return `${
+      spriteDetails()[currentIndex()].pageDim.x - (offsetX() + 1) * spriteDetails()[currentIndex()].tileDim.x + dimX()
+    }px ${
+      spriteDetails()[currentIndex()].pageDim.y - (offsetY() + 1) * spriteDetails()[currentIndex()].tileDim.y + dimY()
     }px`;
   });
   return (
     <Show
-      when={currentSettings.displayGraphics && spriteDetails().graphicFilePath.length > 0}
+      when={currentSettings.displayGraphics && spriteDetails()[currentIndex()].graphicFilePath.length > 0}
       fallback={
         <div class='tooltip' data-tip={graphics.loading ? 'Loading graphics..' : 'No Graphics Found'}>
           <div
             class={`border-2 rounded-lg border-accent bg-black/50 ${props.class}`}
-            data-parsed={JSON.stringify(spriteDetails())}
             style={{
               width: '32px',
               height: '32px',
@@ -135,9 +163,7 @@ const SpriteImage: Component<SpriteImageProps> = (props) => {
           </div>
         </div>
       }>
-      <div
-        class={`border-2 rounded-lg border-accent bg-black/50 ${props.class}`}
-        data-parsed={JSON.stringify(spriteDetails())}>
+      <div class={`border-2 rounded-lg border-accent bg-black/50 ${props.class}`}>
         <div
           style={{
             width: `${dimX()}px`,
