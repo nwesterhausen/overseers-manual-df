0ee4e326bcac193aa4425e3df96b1783daca7e60
Nicholas Westerhausen <2317381+nwesterhausen@users.noreply.github.com>
2023-12-09 15:10:35 -0500

    fix: update to latest solid and router

--- src/index.tsx
+++ src/index.tsx
@@ -2,8 +2,7 @@
 import { render } from 'solid-js/web';
 // import './sass/main.scss';
 
-import { Router } from '@solidjs/router';
-import App from './App';
+import { Route, Router } from '@solidjs/router';
 import { RawsProvider } from './providers/RawsProvider';
 import { SearchProvider } from './providers/SearchProvider';
 import { SettingsProvider } from './providers/SettingsProvider';
@@ -11,22 +10,36 @@ import { SettingsProvider } from './providers/SettingsProvider';
 import { attachConsole } from '@tauri-apps/plugin-log';
 import { UpdateProvider } from './providers/UpdateProvider';
 
+import { lazy } from 'solid-js';
+import MenuBar from './components/menu/MenuBar';
+import ReferenceManual from './pages/ReferenceManual';
+const Settings = lazy(() => import('./pages/Settings'));
+const About = lazy(() => import('./pages/About'));
+const UpdateDetails = lazy(() => import('./pages/UpdateDetails'));
+
 // Attach our console to the tauri logs
 attachConsole().catch(console.error);
 
+const App = (props) => (
+  <SettingsProvider>
+    <SearchProvider>
+      <RawsProvider>
+        <UpdateProvider>
+          <MenuBar>{props.children}</MenuBar>
+        </UpdateProvider>
+      </RawsProvider>
+    </SearchProvider>
+  </SettingsProvider>
+);
+
 render(
   () => (
-    <Router>
-      <SettingsProvider>
-        <SearchProvider>
-          <RawsProvider>
-            <UpdateProvider>
-              <App />
-            </UpdateProvider>
-          </RawsProvider>
-        </SearchProvider>
-      </SettingsProvider>
+    <Router root={App}>
+      <Route path='/' component={ReferenceManual} />
+      <Route path='/settings' component={Settings} />
+      <Route path='/about' component={About} />
+      <Route path='/update-details' component={UpdateDetails} />
     </Router>
   ),
-  document.getElementById('root') as HTMLElement,
+  document.getElementById('root')!,
 );
