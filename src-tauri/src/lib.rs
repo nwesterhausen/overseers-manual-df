e6dec67775439baf4f1e4634f47eb66a3258a55d
Nick Westerhausen <nwesterhausen@gmail.com>
2026-01-17 17:51:30 -0500

    add updater plugin back

--- src-tauri/src/lib.rs
+++ src-tauri/src/lib.rs
@@ -31,6 +31,7 @@ static LOG_GUARDS: OnceLock<(WorkerGuard, WorkerGuard)> = OnceLock::new();
 #[cfg_attr(mobile, tauri::mobile_entry_point)]
 pub fn run() {
     tauri::Builder::default()
+        .plugin(tauri_plugin_updater::Builder::new().build())
         .plugin(tauri_plugin_fs::init())
         .plugin(tauri_plugin_opener::init())
         .invoke_handler(tauri::generate_handler![
